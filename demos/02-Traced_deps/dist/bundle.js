a9340c971dcab3f81d9b8cab5650400aaa9c874f
jods4 <joel.dumas@elca.ch>
2017-03-04 16:00:27 +0100

    Updated deps to published webpack plugins

--- demos/02-Traced_deps/dist/bundle.js
+++ demos/02-Traced_deps/dist/bundle.js
@@ -22420,7 +22420,7 @@ module.exports = "<template>\r\n  <div class=\"tag-header\">\r\n    HELLO<br>\r\
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_aurelia_pal__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_aurelia_framework__ = __webpack_require__("aurelia-framework");
 /* harmony export (immutable) */ __webpack_exports__["configure"] = configure;
 
 function configure(aurelia) {
