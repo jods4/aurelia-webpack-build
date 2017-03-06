2387b371a69f4b8fa3d1d5e70f9a3245a55fadf7
jods4 <joel.dumas@elca.ch>
2017-03-06 21:15:22 +0100

    Remove unnecessary usings

--- demos/06-ASPNET/Startup.cs
+++ demos/06-ASPNET/Startup.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace AureliaDemo
 {
-  public class Startup
+    public class Startup
   {
     public void ConfigureServices(IServiceCollection services)
     {
