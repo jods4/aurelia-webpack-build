0df3ab691427be22953cdda3d6cbc2b819944dde
Joel Dumas <joel.dumas@elca.ch>
2017-03-10 18:32:11 +0100

    Use /dist/ instead of dist in publicPath Although the demos did work, some more advanced scenarios don't work otherwise.

--- demos/03-Code_splits/webpack.config.js
+++ demos/03-Code_splits/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 
   output: {
     path: path.resolve(__dirname, "dist"),
-    publicPath: "dist",
+    publicPath: "/dist/",
     filename: "[name].js",    
     chunkFilename: "[name].js"
   },
--- demos/04-Small_ES6_build/webpack.config.js
+++ demos/04-Small_ES6_build/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 
   output: {
     path: path.resolve(__dirname, "dist"),
-    publicPath: "dist",
+    publicPath: "/dist/",
     filename: "bundle.js",    
   },
 
